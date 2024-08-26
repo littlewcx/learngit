d4c25f37cf4fe07951e17d5a7f3d805690f4bfa7
Hank <1109wcx1109@gmail.com>
2024-08-26 08:30:33 +0800

    addnew

--- Test/Test/Test.cpp
+++ Test/Test/Test.cpp
@@ -1,20 +1,52 @@
-﻿// Test.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    const string s = "sss";
+    std::cout << "Hello World!\\"+s  <<std::endl;
+
+
+    int age = 7;
+    std::cout << "Hello"<<age<<"World!\\" + s << std::endl;
+    char aa = 'd'; 
+    int g=2;
+
+    /*
+    switch 用法:
+
+    switch(g){
+    
+        case  :
+
+            break;
+        default:
+     
+    
+    }
+
+    
+    */
+    switch (g) {
+        case 0:
+            std::cout << "Hello" <<g<< "World!\\" + s << std::endl;
+            break;
+        case 1:
+            std::cout << "Hello" << g << "World!\\" + s << std::endl;
+            break;
+        case 2:
+            std::cout << "Hello" << g << "World!\\" + s << std::endl;
+            break;
+        default:
+            std::cout << "Hello" << g << "World!\\" + s << std::endl;
+
+
+    }
+
+       
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
